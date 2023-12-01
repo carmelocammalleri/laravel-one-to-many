c315960b0548efbe045f2009c80e50c91c729208
Melozzo <carmecamma1995@gmail.com>
2023-12-01 16:57:41 +0100

    [add]: type in show project

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,9 +4,11 @@
     <div class="container">
         <h1>Dettaglio progetto</h1>
         <h3>{{$project->name}}</h3>
+        @if ($project->type)
+        <p>Tipo lavoro: <strong> {{ $project->type->name}}</strong> </p>
+        @endif
         <p>{{$project->description}}</p>
         <p>Url: {{$project->web_site}}</p>
-        <p>Linguaggio usato: {{$project->type}}</p>
         <p>Tecnologia usata: {{$project->tecnology}}</p>
         <p>Creazione: {{$project->date_creation}}</p>
     </div>
