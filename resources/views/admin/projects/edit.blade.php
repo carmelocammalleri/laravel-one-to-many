99abd6f09de7f4a1a2a4114a13d52c61d2849033
Melozzo <carmecamma1995@gmail.com>
2023-12-03 15:55:25 +0100

    [fix]: controll editor

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -3,26 +3,66 @@
 @section('content')
 <div class="container">
     <h1>{{$title}}</h1>
+
+    {{-- errori visuale --}}
+    @if($errors->any())
+      <div class="alert alert-danger" role="alert" >
+          <ul>
+              @foreach($errors->all() as $error)
+                  <li>{{$error}}</li>
+              @endforeach
+          </ul>
+      </div>
+    @endif
+    {{-- /errori visuale --}}
+
+
     <form action="{{route('admin.projects.update', $project)}}" method="POST">
         @csrf
         @method('PUT')
+
+        {{-- name form --}}
         <div class="mb-3">
-          <label for="name" class="form-label">Nome</label>
-          <input type="text" class="form-control" id="name" name="name" value="{{ old('name', $project->name) }}">
+            <label for="name" class="form-label">Nome *</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name', $project->name) }}">
+            @error('name')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
+        {{-- /name form --}}
+
+        {{-- date_creation form --}}
         <div class="mb-3">
-          <label for="date_creation" class="form-label">Data di creazione</label>
-          <input type="date" class="form-control" id="date_creation" name="date_creation" value="{{ old('date_creation', $project->date_creation) }}">
+          <label for="date_creation" class="form-label">Data di creazione *</label>
+          <input type="date" class="form-control @error('date_creation') is-invalid @enderror" id="date_creation" id="date_creation" name="date_creation" value="{{ old('date_creation', $project->date_creation) }}">
+          @error('date_creation')
+            <p class="text-danger">{{ $message }}</p>
+          @enderror
         </div>
+        {{-- /date_creation form --}}
+
+        {{-- web_site form --}}
         <div class="mb-3">
             <label for="web_site" class="form-label">Url</label>
-            <input type="url" class="form-control" id="web_site" name="web_site" value="{{ old('web_site', $project->web_site) }}">
+            <input type="url" class="form-control @error('web_site') is-invalid @enderror" id="web_site" name="web_site" value="{{ old('web_site', $project->web_site) }}">
+            @error('web_site')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
+        {{-- /web_site form --}}
+
+
+        {{-- tecnology form --}}
         <div class="mb-3">
             <label for="tecnology" class="form-label">Tecnologia</label>
-            <input type="text" class="form-control" id="tecnology" name="tecnology" value="{{ old('tecnology',$project->tecnology) }}">
+            <input type="text" class="form-control @error('tecnology') is-invalid @enderror" id="tecnology" name="tecnology" value="{{ old('tecnology',$project->tecnology) }}">
+            @error('tecnology')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
+        {{-- /tecnology form --}}
 
+        {{-- type select options --}}
         <div class="mb-3">
             <label for="type_id" class="form-label">Tipo</label>
             <select id="type_id" name="type_id">
@@ -32,11 +72,17 @@
                 @endforeach
             </select>
         </div>
+        {{-- /type select options --}}
 
+        {{-- description form --}}
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
-            <textarea type="text" class="form-control" id="description" name="description" value="{{ old('description', $project->description) }}"></textarea>
+            <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" id="description" name="description" value="{{ old('description', $project->description) }}"></textarea>
+            @error('description')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
+        {{-- /description form --}}
 
         <button type="submit" class="btn btn-secondary ">Invia</button>
         <button type="reset" class="btn btn-danger">Annulla</button>
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -26,7 +26,7 @@
                   <td>{{ $project->type->name ?? '-'}}</td>
                   <td>
                     <a class="btn btn-success" href="{{ route('admin.projects.show', $project->id)}}"><i class="fa-solid fa-eye"></i></a>
-                    <a class="btn btn-warning" href="{{route('admin.projects.edit', $project)}}"><i class="fa-regular fa-pen-to-square"></i></a>
+                    <a class="btn btn-warning text-white" href="{{route('admin.projects.edit', $project)}}"><i class="fa-regular fa-pen-to-square"></i></a>
                     <form class="d-inline-block"
                         action="{{ route('admin.projects.destroy', $project->id) }}"
                        method="POST"
